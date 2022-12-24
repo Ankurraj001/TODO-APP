58a9472728a93d235107f7cdbd244afb04f2b9ba
ankurraj <ankur.raj@xpressbees.com>
2022-12-25 04:17:11 +0530

    Redux added

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Todo from "./Components/Todo";
+import Add from "./Components/Add";
 
 function App() {
   return (
     <div className="App">
       <Todo />
+      <Add />
     </div>
   );
 }
